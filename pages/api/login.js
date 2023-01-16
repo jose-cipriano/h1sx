b5bdd902de2285f452d24cc772e40e84bf1267f8
sash-257defi <sashcy@yahoo.com>
2023-01-16 16:52:08 +0200

    initial

---
+++ pages/api/login.js
@@ -0,0 +1,33 @@
+import clientPromise from '../../lib/mongodb'
+
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../lib/session'
+
+async function handler(req, res) {
+    try {
+        const client = await clientPromise
+        const db = client.db('Next-Dash')
+        const { username, password } = await req.body
+        const oneUser = await db.collection('Users').find({ username }).toArray()
+        if (!oneUser.length) {
+            res.status(200).json({ success: false, message: 'Invalid credentials' })
+        } else {
+            if (oneUser[0].password !== password) {
+                res.status(200).json({ success: false, message: 'Invalid Password' })
+            }
+        }
+
+        const user = { isLoggedIn: true }
+        req.session.user = user
+        await req.session.save()
+        res.json({
+            success: true,
+            message: 'successfully signed in',
+            user,
+        })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
+
+export default withIronSessionApiRoute(handler, sessionOptions)
